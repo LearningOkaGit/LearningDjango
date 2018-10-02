f3f1e2ff0346d6962b179b1ce76cc21d91168c92
Yuji <okamotoxp@yahoo.co.jp>
2018-10-02 09:14:15 +0900

    手順 3-2: アプリからの静的ファイルを提供する

--- BasicProject/BasicProject/settings.py
+++ BasicProject/BasicProject/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'app',
     # Add your apps here to enable them
+    'HelloDjangoApp',
+    # Other entries...
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
